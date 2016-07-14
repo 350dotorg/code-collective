b9c146a9fde4179a2418238bcefc3ae453592ca9
kprakobkit <kprakobkit@gmail.com>
2016-07-13 19:12:27 -0700

    Errol & Peter - Fixes modal; opens modal on click, and shows content

--- actionkit-popup-form/src/demo.js
+++ actionkit-popup-form/src/demo.js
@@ -2,9 +2,7 @@ $(document).ready(function() {
   var modalHook = '.js-actionkit-popup-form';
   var modalTemplate = '#actionkit-popup-form';
   var actionKitPage = $(modalHook).data('actionkit-page');
-  
-  $(modalHook).on('click', function() {
-    $(modalTemplate).append('<h1>' + actionKitPage + '</h1>');
-    $(modalHook).modal();
-  });
+  var modalContent = $('#modal-content')
+
+  $(modalHook).modal();
 });
