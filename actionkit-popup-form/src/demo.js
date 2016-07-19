92f0bc407773c4729805fb093f23efa48ab3b3d2
kprakobkit <kprakobkit@gmail.com>
2016-07-19 12:44:12 -0700

    Peter - parses field data into a form inside the modal

--- actionkit-popup-form/src/demo.js
+++ actionkit-popup-form/src/demo.js
@@ -4,8 +4,54 @@ $(document).ready(function() {
   var actionKitPage = $(modalHook).data('actionkit-page');
   var modalContent = $('#modal-content')
 
-  $(modalHook).modal();
+  function initializeModal() {
+    $(modalHook).modal();
+  }
 
-  var foo = $.getJSON("http://act.350.org/act/sweden-kiitg-pledge/?template_set=json_form_data&jsonp=?", function(result){
-  });
+  getFormData()
+    .then(parseFields)
+    .then(addFieldsTo(modalContent))
+    .then(initializeModal);
 });
+
+function toFormField(fieldData) {
+  const label = $(`<label>${fieldData.label}:  </label>`);
+
+  return label.append($(fieldData.tag));
+}
+
+function createTitle(title) {
+  return $(`<h1>${title}</h1>`);
+}
+
+function createIntroText(text) {
+  return $(text);
+}
+
+function createForm(fields) {
+  return $('<form></form>').append(fields);
+}
+
+function parseFields(data) {
+  return {
+    title: createTitle(data.title),
+    introText: createIntroText(data.introduction_text),
+    form: createForm(data.fields.map(toFormField))
+  };
+}
+
+function addFieldsTo(container) {
+  return function(fields) {
+    for (field in fields) {
+      container.append(fields[field]);
+    }
+  }
+}
+
+function getFormData() {
+  return new Promise(
+    function(resolve, reject) {
+      $.getJSON("http://act.350.org/act/sweden-kiitg-pledge/?template_set=json_form_data&jsonp=?", resolve, reject)
+    }
+  );
+}
